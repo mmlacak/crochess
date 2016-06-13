8b07a51e154c757e18dd3598da9e351d8a826e2c
Mario Mlačak <mmlacak@gmail.com>
2016-06-14 00:32:53 +0200

    Moved default consts into Arrow class, added coords type for Arrow.

--- book/py/mark.py
+++ book/py/mark.py
@@ -4,22 +4,38 @@
 # Copyright (c) 2016 Mario Mlačak, mmlacak@gmail.com
 # Licensed under 3-clause (modified) BSD license. See LICENSE.txt for details.
 
-
 import pixel_math as pm
 
+class CoordinateType(int):
+    Pixel = 1
+    FieldIndex = 2
 
-DEFAULT_FOREGROUND_COLOR = "#FFFFFF"
-DEFAULT_BACKGROUND_COLOR = "#000000"
-DEFAULT_INVERSE_ARROW_WIDTH_RATIO = 12.0 # Compared to field size.
-DEFAULT_POINTY_ARROW_BIT_RATIO = 80.0 # 1.5 # Compared to arrow width.
+    def __new__(cls, value):
+        if CoordinateType._is_valid(value):
+            return super(CoordinateType, cls).__new__(cls, value)
+        else:
+            raise ValueError("No such a coordinate type, received '%s'." % (str(value), ))
 
+    @staticmethod
+    def _is_valid(coord_type):
+        return CoordinateType.Pixel <= coord_type <= CoordinateType.FieldIndex
+
+    def is_valid(self):
+        return CoordinateType._is_valid(self)
 
 class Arrow(object):
+
+    DEFAULT_FOREGROUND_COLOR = "#FFFFFF"
+    DEFAULT_BACKGROUND_COLOR = "#000000"
+    DEFAULT_INVERSE_ARROW_WIDTH_RATIO = 12.0 # Compared to field size.
+    DEFAULT_POINTY_ARROW_BIT_RATIO = 1.5 # Compared to arrow width. # 80.0
+
     def __init__(self, start_x, start_y, end_x, end_y, \
-                 fg_color=DEFAULT_FOREGROUND_COLOR, \
-                 bg_color=DEFAULT_BACKGROUND_COLOR, \
-                 inv_width_ratio=DEFAULT_INVERSE_ARROW_WIDTH_RATIO, \
-                 pointy_bit_ratio=DEFAULT_POINTY_ARROW_BIT_RATIO):
+                 coord_type=CoordinateType.FieldIndex, \
+                 fg_color=None, \
+                 bg_color=None, \
+                 inv_width_ratio=None, \
+                 pointy_bit_ratio=None):
         # self.start_x = start_x
         # self.start_y = start_y
         self.start = (start_x, start_y)
@@ -28,11 +44,13 @@ class Arrow(object):
         # self.end_y = end_y
         self.end = (end_x, end_y)
 
-        self.fg_color = fg_color
-        self.bg_color = bg_color
+        self.coord_type = coord_type
 
-        self.inv_width_ratio = inv_width_ratio
-        self.pointy_bit_ratio = pointy_bit_ratio
+        self.fg_color = fg_color or Arrow.DEFAULT_FOREGROUND_COLOR
+        self.bg_color = bg_color or Arrow.DEFAULT_BACKGROUND_COLOR
+
+        self.inv_width_ratio = inv_width_ratio or Arrow.DEFAULT_INVERSE_ARROW_WIDTH_RATIO
+        self.pointy_bit_ratio = pointy_bit_ratio or Arrow.DEFAULT_POINTY_ARROW_BIT_RATIO
 
     def reverse(self):
         # self.start_x, self.end_x = self.end_x, self.start_x
--- book/py/mark_painter.py
+++ book/py/mark_painter.py
@@ -16,12 +16,12 @@ class MarkPainter(bp.BoardPainter):
 
         self.arrows = arrows
 
-    def calc_arrow(self, arrow, inv_width_ratio=None, pointy_bit_ratio=None):
+    def calc_pixel_arrow(self, arrow, inv_width_ratio=None, pointy_bit_ratio=None):
         # inv_width_ratio - compared to field size
-        inv_width_ratio = inv_width_ratio or arrow.inv_width_ratio or mark.DEFAULT_INVERSE_ARROW_WIDTH_RATIO
+        inv_width_ratio = inv_width_ratio or arrow.inv_width_ratio or mark.Arrow.DEFAULT_INVERSE_ARROW_WIDTH_RATIO
 
         # pointy_bit_ratio - compared to arrow width
-        pointy_bit_ratio = pointy_bit_ratio or arrow.pointy_bit_ratio or mark.DEFAULT_POINTY_ARROW_BIT_RATIO
+        pointy_bit_ratio = pointy_bit_ratio or arrow.pointy_bit_ratio or mark.Arrow.DEFAULT_POINTY_ARROW_BIT_RATIO
 
         width = self.field_width_pix / inv_width_ratio
         distance = width / 2.0
@@ -29,7 +29,7 @@ class MarkPainter(bp.BoardPainter):
 
         start_lst = pm.calc_distant_points_on_inverse_line(arrow.start, arrow.end, distance)
 
-        arrow_size = self.board_width_pix / pointy_bit_ratio # pointy_bit_ratio * width
+        arrow_size = pointy_bit_ratio * width # self.board_width_pix / pointy_bit_ratio
         line_division_ratio = abs((length - arrow_size) / arrow_size) # Shouldn't be negative, i.e. outside line segment.
         mid_point = pm.calc_division_point(arrow.start, arrow.end, line_division_ratio)
 
@@ -40,6 +40,18 @@ class MarkPainter(bp.BoardPainter):
         arrow_lst_2 = [ (int(tpl[0]), int(tpl[1])) for tpl in arrow_lst ]
         return arrow_lst_2
 
+    def calc_field_index_arrow(self, arrow, inv_width_ratio=None, pointy_bit_ratio=None):
+# TODO :: calculate from field indexes into pixels ...
+        pass
+
+    def calc_arrow(self, arrow, inv_width_ratio=None, pointy_bit_ratio=None):
+        if arrow.coord_type == mark.CoordinateType.Pixel:
+            return self.calc_pixel_arrow(arrow, inv_width_ratio=inv_width_ratio, pointy_bit_ratio=pointy_bit_ratio)
+        elif arrow.coord_type == mark.CoordinateType.FieldIndex:
+            return self.calc_field_index_arrow(arrow, inv_width_ratio=inv_width_ratio, pointy_bit_ratio=pointy_bit_ratio)
+
+        assert False, "Can't render arrow with coord type '%s'." % str(arrow.coord_type)
+
     def draw_arrow(self, arrow, pc, \
                    fg_color=None, \
                    bg_color=None, \
@@ -47,8 +59,8 @@ class MarkPainter(bp.BoardPainter):
                    pointy_bit_ratio=None):
         points_pix = self.calc_arrow(arrow, inv_width_ratio=inv_width_ratio, pointy_bit_ratio=pointy_bit_ratio)
 
-        fg_color = fg_color or arrow.fg_color or mark.DEFAULT_FOREGROUND_COLOR
-        bg_color = bg_color or arrow.bg_color or mark.DEFAULT_BACKGROUND_COLOR
+        fg_color = fg_color or arrow.fg_color or mark.Arrow.DEFAULT_FOREGROUND_COLOR
+        bg_color = bg_color or arrow.bg_color or mark.Arrow.DEFAULT_BACKGROUND_COLOR
         gc = pc.get_gc_colors(fg_color, bg_color)
         self.draw_polygon_with_background(gc, points_pix)
 
