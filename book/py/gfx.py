aaf8fbc95417f31a06d4432e6f4f0682e3d37839
Mario Mlačak <mmlacak@gmail.com>
2016-11-01 16:26:23 +0100

    Defined graphics include path. Changed all graphics paths to be relative to include path.

--- book/py/gfx.py
+++ book/py/gfx.py
@@ -24,10 +24,10 @@ import debug_
 
 class GfxRender(object):
 
-    DEFAULT_BOARD_RENDERING_SIZE = 8000
-    DEFAULT_MAX_BOARD_VERTICAL_RENDERING_SIZE = 12000 # 50% added to rendering size
-    DEFAULT_PIECE_2x2_RENDERING_SIZE = 3200 # 40% of rendering size
-    DEFAULT_BOARD_LINE_WIDTH = 11 # >= 1 + (6 * rendering size / 5) // 1000
+    DEFAULT_BOARD_RENDERING_SIZE = 2000 # 8000
+    DEFAULT_MAX_BOARD_VERTICAL_RENDERING_SIZE = 3000 # 12000 # 50% added to rendering size
+    DEFAULT_PIECE_2x2_RENDERING_SIZE = 800 # 3200 # 40% of rendering size
+    DEFAULT_BOARD_LINE_WIDTH = 4 # 11 # >= 1 + (6 * rendering size / 5) // 1000
     DEFAULT_PATH = '../tmp/' # '../gfx/'
     DEFAULT_FILE_EXT = '.png'
     DEFAULT_FILE_TYPE = 'png'
