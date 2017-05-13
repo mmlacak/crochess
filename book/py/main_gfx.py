5133fa1d924591ba84aa3aea00c61cc12d5f1ac3
Mario Mlačak <mmlacak@gmail.com>
2017-05-13 14:22:06 +0200

    Changed scenes rendering options. Added convenience shell scripts.

--- book/py/main_gfx.py
+++ book/py/main_gfx.py
@@ -21,13 +21,13 @@ Licensed under 3-clause (modified) BSD license. See LICENSE.txt for details.''')
     size.add_argument('-f', '--final', action='store_true', default=False, help='render in final size (huge!)')
     size.add_argument('-d', '--draft', action='store_true', default=False, help='render in draft size')
 
-    collections = parser.add_argument_group(title='Collections', description='Define which collections will be rendered. Provide at least one of the options bellow.')
+    collections = parser.add_argument_group(title='collections', description='Define which collections will be rendered. Provide at least one of the options bellow.')
                 # parser.add_mutually_exclusive_group(required=True)
     collections.add_argument('-a', '--all', action='store_true', default=False, help='render all collections')
     collections.add_argument('-b', '--boards', action='store_true', default=False, help='render initial position boards')
     collections.add_argument('-p', '--pieces', action='store_true', default=False, help='render newly introduced pieces')
-    collections.add_argument('-s', '--scenes', action='store_true', default=False, help='render recent scenes, move examples')
-    collections.add_argument('-t', '--all_scenes', action='store_true', default=False, help='render all scenes, move examples')
+    collections.add_argument('-r', '--recent_scenes', action='store_true', default=False, help='render recent scenes, move examples')
+    collections.add_argument('-s', '--scenes', action='store_true', default=False, help='render all scenes, move examples')
     collections.add_argument('-c', '--castlings', action='store_true', default=False, help='render castling examples')
     collections.add_argument('-e', '--en_passant', action='store_true', default=False, help='render en passant examples')
 
@@ -40,7 +40,6 @@ Licensed under 3-clause (modified) BSD license. See LICENSE.txt for details.''')
 
     fs.create_subfolders('../gfx')
 
-#     is_final = args.final
     render = GfxRender()
 
     if args.all or args.boards:
@@ -49,8 +48,8 @@ Licensed under 3-clause (modified) BSD license. See LICENSE.txt for details.''')
     if args.all or args.pieces:
         render.render_all_newly_introduced_pieces()
 
-    if args.all or args.all_scenes or args.scenes:
-        render.render_all_example_scenes(do_all_scenes=args.all_scenes)
+    if args.all or args.scenes or args.recent_scenes:
+        render.render_all_example_scenes(do_all_scenes=args.scenes)
 
     if args.all or args.castlings:
         render.render_all_castling_scenes()
