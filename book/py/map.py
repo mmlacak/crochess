21ca6a88ab58c59fa4faa61d227edd4e7bdd6d65
Mario Mlačak <mmlacak@gmail.com>
2018-03-19 01:29:59 +0100

    Added map.

---
+++ book/py/map.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018 Mario Mlačak, mmlacak@gmail.com
+# Licensed under 3-clause (modified) BSD license. See LICENSE.txt for details.
+
+
+class Map(dict):
+    def __getattr__(self, name):
+        return self[name]
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        del self[name]
+
+
+if __name__ == '__main__':
+    print
+    # m = Map({'foo' : 11, 'bar' : 42, 'baz' : 77})
+    m = Map(foo=11, bar=42, baz=77)
+    print m
+
+    print
+    m.new = 33
+    print m
+
+    print
+    m.foo = 99
+    print m
+
+    print
+    del m.bar
+    print m
+
+    print
