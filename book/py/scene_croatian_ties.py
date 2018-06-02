f346b69a054e899759580cd1d52c7b511866da63
Mario Mlačak <mmlacak@gmail.com>
2018-06-02 14:47:08 +0200

    Added Pegasus direction scene.

--- book/py/scene_croatian_ties.py
+++ book/py/scene_croatian_ties.py
@@ -6,7 +6,7 @@
 
 
 from util import in_range
-from gen_steps import DEFAULT_KNIGHT_REL_MOVES, call_gen, get_gen_steps_prev, get_gen_multi_steps
+from gen_steps import DEFAULT_KNIGHT_REL_MOVES, call_gen, get_gen_steps, get_gen_steps_prev, get_gen_multi_steps
 
 from piece import PieceType
 from board import BoardType, Board
@@ -31,3 +31,48 @@ class SceneCroatianTiesMixin(Scene):
             i += 1
 
         return "scn_ct_pegasus_initial"
+
+    def scn_ct_pegasus_direction(self, bt=BoardType.CroatianTies):
+        self.init_scene(bt)
+
+        start = (2, 1)
+        self.board.set_piece(*start, piece=PieceType(PieceType.Pegasus))
+
+        # main direction, i.e. <1, 2>
+        coords = call_gen( get_gen_steps_prev(start=start, rel=(1, 2)) )
+        self.append_arrow( *coords() )
+        self.append_arrow( *coords() )
+        self.append_arrow( *coords() )
+        self.append_arrow( *coords() )
+
+        coords = call_gen( get_gen_steps(start=start, rel=(1, 2)) )
+        self.append_text("1", *coords(), corner=Corner.UpperLeft)
+        self.append_text("2", *coords(), corner=Corner.UpperLeft)
+        self.append_text("3", *coords(), corner=Corner.UpperLeft)
+        self.append_text("4", *coords(), corner=Corner.UpperLeft)
+
+        # direction 2a, i.e. <2, 1>
+        self.append_arrow(4, 5, 6, 6, mark_type=MarkType.Blocked)
+        self.append_text("2a", 6, 6, corner=Corner.UpperRight, mark_type=MarkType.Blocked, rect=(0.05, 1.0, 0.6, 0.45))
+
+        # direction 2b, i.e. <2, -1>
+        self.append_arrow(4, 5, 6, 4, mark_type=MarkType.Blocked)
+        self.append_text("2b", 6, 4, corner=Corner.UpperRight, mark_type=MarkType.Blocked, rect=(0.05, 1.0, 0.6, 0.45))
+
+        # direction 2c, i.e. <1, -2>
+        self.append_arrow(4, 5, 5, 3, mark_type=MarkType.Blocked)
+        self.append_text("2c", 5, 3, corner=Corner.UpperRight, mark_type=MarkType.Blocked, rect=(0.05, 1.0, 0.6, 0.45))
+
+        # direction 2d, i.e. <-2, -1>
+        self.append_arrow(4, 5, 2, 4, mark_type=MarkType.Blocked)
+        self.append_text("2d", 2, 4, corner=Corner.UpperLeft, mark_type=MarkType.Blocked)
+
+        # direction 2e, i.e. <-2, 1>
+        self.append_arrow(4, 5, 2, 6, mark_type=MarkType.Blocked)
+        self.append_text("2e", 2, 6, corner=Corner.UpperLeft, mark_type=MarkType.Blocked)
+
+        # direction 2f, i.e. <-1, 2>
+        self.append_arrow(4, 5, 3, 7, mark_type=MarkType.Blocked)
+        self.append_text("2f", 3, 7, corner=Corner.UpperRight, mark_type=MarkType.Blocked, rect=(0.05, 1.0, 0.6, 0.45))
+
+        return 'scn_ct_pegasus_direction'
