46cd79c3a025a90825ec0350f8c9f335053784c9
Mario Mlačak <mmlacak@gmail.com>
2020-05-26 11:10:57 +0200

    Added commit count to colophon.

--- book/py/book_log.py
+++ book/py/book_log.py
@@ -4,9 +4,6 @@
 # Copyright (c) 2020 Mario Mlačak, mmlacak@gmail.com
 # Licensed under 3-clause (modified) BSD license. See LICENSE.txt for details.
 
-# TODO
-# git rev-list --count HEAD
-
 import os
 import os.path
 import re
@@ -15,7 +12,10 @@ import time
 GIT_FOLDER = '.git'
 GIT_REF_FILE_NAME = 'HEAD'
 GIT_LOG_FOLDER = 'logs'
-GIT_LOG_FILE_NAME = 'HEAD'
+# GIT_LOG_FILE_NAME = 'HEAD'
+
+REF_LINE_REGEXP = '''ref: (?P<path>.*)'''
+REF_LINE_REGEXP_OBJ = re.compile(REF_LINE_REGEXP)
 
 LOG_LINE_REGEXP = '''(?P<parent>[\da-fA-F]{40})\s(?P<id>[\da-fA-F]{40})\s(?P<commiter>.*)\s(?P<time>\d{10})\s\+\d{4}\scommit:\s(?P<msg>.*)'''
 LOG_LINE_REGEXP_OBJ = re.compile(LOG_LINE_REGEXP)
@@ -34,21 +34,52 @@ def get_project_path(root_path=None):
     path = os.path.normcase(path)
     return path
 
-def get_full_log_path(root_path=None, git_dir=GIT_FOLDER, git_log_dir=GIT_LOG_FOLDER, filename=GIT_LOG_FILE_NAME):
+def get_ref_log_path(root_path=None, git_dir=GIT_FOLDER, git_ref_file_name=GIT_REF_FILE_NAME):
     path = get_project_path(root_path=root_path)
-    path = os.path.join(path, git_dir, git_log_dir, filename)
+    path = os.path.join(path, git_dir, git_ref_file_name)
     return path
 
-def get_log_entry(root_path=None):
-    log_path = get_full_log_path(root_path=root_path)
+def get_full_log_path(root_path=None, git_dir=GIT_FOLDER, git_log_folder=GIT_LOG_FOLDER, git_log_paths=None):
+    if git_log_paths is None:
+        return None
+    path = get_project_path(root_path=root_path)
+    path = os.path.join(path, git_dir, git_log_folder, *git_log_paths)
+    return path
+
+def get_ref_paths(root_path=None):
+    ref_path = get_ref_log_path(root_path=root_path)
+    first_line = None
+
+    try:
+        with open(ref_path, 'r') as ref:
+            first_line = ref.readline()
+    except:
+        return None
+
+    if first_line is not None:
+        matches = REF_LINE_REGEXP_OBJ.match(first_line)
+
+    if matches is not None:
+        results = matches.group('path') 
+
+        paths = results.split('/')
+        results = (results, paths, )
+
+    return paths # results
+
+def get_log_entry(root_path=None, git_log_paths=None):
+    log_path = get_full_log_path(root_path=root_path, git_log_paths=git_log_paths)
     last_line = None
     matches = None
     results = None
+    counter = None
 
     try:
         with open(log_path, 'r') as log:
+            counter = 0
             for line in log:
                 last_line = line
+                counter += 1
     except:
         return None
 
@@ -67,7 +98,7 @@ def get_log_entry(root_path=None):
         now_long = time.strftime('%Y-%m-%d %H:%M:%S UTC', now) # e.g. '2020-05-17 02:11:11 UTC'
         now_short = time.strftime('%Y-%m-%d', now) # e.g. '2020-05-17'
 
-        results = results + (long_, short_, now_long, now_short) 
+        results = results + (long_, short_, now_long, now_short, counter) 
 
     return results
 
@@ -76,26 +107,30 @@ def get_full_tex_path(root_path=None, tex_dir=BOOK_TEX_FOLDER, tex_name=BOOK_TEX
     path = os.path.join(path, tex_dir, tex_name)
     return path
 
-def change_line_if_marked(line, id_, long_, short, now_long, now_short):
+def change_line_if_marked(line, id_, long_, short, now_long, now_short, counter):
     new = line
     if 'last-commit-id-place-marker' in line:
         new = '    %% %s \\\\ %% last-commit-id-place-marker\n' % id_
     elif 'last-commit-date-time-place-marker' in line:
         new = '    %% %s \\\\ %% last-commit-date-time-place-marker\n' % long_
-    elif 'new-commit-date-time-place-marker' in line:
-        new = '    %s \\\\ [2.0em] %% new-commit-date-time-place-marker\n' % now_long
+    elif 'new-commit-count-date-time-place-marker' in line:
+        new = '    %i || %s \\\\ [2.0em] %% new-commit-count-date-time-place-marker\n' % (counter+1, now_long)
     elif 'new-commit-date-place-marker' in line:
-        new = '    %s \\\\ %% new-commit-date-place-marker\n' % now_short # short
+        new = '    %s \\\\ %% new-commit-date-place-marker\n' % now_short 
     elif 'new-commit-date-small-place-marker' in line:
-        new = '    \small{%s} \\\\ [0.5em] %% new-commit-date-small-place-marker\n' % now_short # short
+        new = '    \small{%s} \\\\ [0.5em] %% new-commit-date-small-place-marker\n' % now_short 
     return new
 
 def replace_log_entries(root_path=None):
-    log_results = get_log_entry(root_path=root_path)
+    ref_paths = get_ref_paths(root_path=root_path) # :: [ path_str_1, ... ] # :: ( full_path_str, [ path_str_1, ... ] )
+    if ref_paths is None:
+        return
+
+    log_results = get_log_entry(root_path=root_path, git_log_paths=ref_paths)
     if log_results is None:
         return
 
-    parent, id_, commiter, time_, msg, long_, short, now_long, now_short = log_results
+    parent, id_, commiter, time_, msg, long_, short, now_long, now_short, counter = log_results
     orig_path = get_full_tex_path(root_path=root_path)
     ignore_path = get_full_tex_path(root_path=root_path, tex_name=BOOK_IGNORE_TEX_FILE_NAME)
 
@@ -107,7 +142,7 @@ def replace_log_entries(root_path=None):
     with open(ignore_path, 'r') as old:
         with open(orig_path, 'w') as orig:
             for line in old:
-                new = change_line_if_marked(line, id_, long_, short, now_long, now_short)
+                new = change_line_if_marked(line, id_, long_, short, now_long, now_short, counter)
                 orig.write(new)
 
 
