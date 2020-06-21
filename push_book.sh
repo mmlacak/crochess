ad1c9da0a202258ee76af61d6833bbfe4fe5b379
Mario Mlačak <mmlacak@gmail.com>
2020-06-21 06:06:33 +0200

    Commented that escaped double quotes do not work properly. -- push_book.sh book/crochess.tex

--- push_book.sh
+++ push_book.sh
@@ -3,12 +3,13 @@
 # Copyright (c) 2010 - 2020 Mario Mlačak, mmlacak@gmail.com
 # Licensed under 3-clause (modified) BSD license. See LICENSE.txt for details.
 
-# Do escape double quotes (") and apostrophes (') around commit message,
-# but not file paths:
+# Do not escape double quotes (") and apostrophes (') around commit message, file paths:
 #
-# $ ./push_book.sh -m \"Comment workaround for terminal argument parsing.\" -- "push_book.sh"
+# $ ./push_book.sh -m "Commented that escaped double quotes do not work properly." -- "push_book.sh" "book/crochess.tex"
 
-# If not using -a option, check if files are added to stage, not just modified!
+# If not using -a option:
+# 1. check if files are added to the stage (git add), not just modified
+# 2. add "book/crochess.tex" to the list of files to commit
 
 python2 book/py/book_log.py
 
