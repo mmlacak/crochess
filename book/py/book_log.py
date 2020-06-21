a3dc99142ea058b82b8f79cefc8dbc48949e190d
Mario Mlačak <mmlacak@gmail.com>
2020-06-21 19:34:09 +0200

    Moved last commit id, date into colophon.

--- book/py/book_log.py
+++ book/py/book_log.py
@@ -71,12 +71,12 @@ def change_line_if_marked(line, id_, last_commit_date_time, now_long, now_short,
     new = line
     if 'new-commit-count-date-time-branch-place-marker' in line:
         new = '    %i \\textperiodcentered \\textperiodcentered \\textperiodcentered ~%s \\textperiodcentered \\textperiodcentered \\textperiodcentered ~%s \\\\ [2.0em] %% new-commit-count-date-time-branch-place-marker\n' % (counter+1, now_long, branch)
-#     elif 'last-commit-id-place-marker' in line:
-#         new = '    %% last: %s \\\\ [2.0em] %% last-commit-id-place-marker\n' % id_
-    elif 'last-commit-pdfinfo-place-marker' in line:
-        new = '            pdfkeywords={chess, variants, %s, %s}, %% last-commit-pdfinfo-place-marker\n' % (id_, last_commit_date_time)
-#     elif 'last-commit-date-time-place-marker' in line:
-#         new = '    %% %s \\\\ %% last-commit-date-time-place-marker\n' % last_commit_date_time
+    elif 'last-commit-id-place-marker' in line:
+        new = '        %s \\\\ %% last-commit-id-place-marker\n' % id_
+    # elif 'last-commit-pdfinfo-place-marker' in line:
+    #     new = '            pdfkeywords={chess, variants, %s, %s}, %% last-commit-pdfinfo-place-marker\n' % (id_, last_commit_date_time)
+    elif 'last-commit-date-time-place-marker' in line:
+        new = '        %s \\\\ %% last-commit-date-time-place-marker\n' % last_commit_date_time
     elif 'new-commit-date-place-marker' in line:
         new = '    %s \\\\ %% new-commit-date-place-marker\n' % now_short
     elif 'new-commit-date-small-place-marker' in line:
