a93fd22acbccfea7755ace27fc43a5146c97996f
Mario Mlačak <mmlacak@gmail.com>
2020-06-21 06:13:39 +0200

    Commented only useable case.

--- push_book.sh
+++ push_book.sh
@@ -3,13 +3,9 @@
 # Copyright (c) 2010 - 2020 Mario Mlačak, mmlacak@gmail.com
 # Licensed under 3-clause (modified) BSD license. See LICENSE.txt for details.
 
-# Do not escape double quotes (") and apostrophes (') around commit message, file paths:
+# Do use only with -a option, and no file paths:
 #
-# $ ./push_book.sh -m "Commented that escaped double quotes do not work properly." -- "push_book.sh" "book/crochess.tex"
-
-# If not using -a option:
-# 1. check if files are added to the stage (git add), not just modified
-# 2. add "book/crochess.tex" to the list of files to commit
+# $ ./push_book.sh -am "Commented only useable case."
 
 python2 book/py/book_log.py
 
