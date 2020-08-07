acaf46b6c97779d59970f70a46ce0673ce45fb66
Mario Mlačak <mmlacak@gmail.com>
2020-08-08 01:54:17 +0200

    Converted pixel math module to Python 3.

--- book/py/pixel_math.py
+++ book/py/pixel_math.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2016, 2020 Mario Mlačak, mmlacak@gmail.com
@@ -39,6 +39,24 @@ def q_same_rounded_floats(num0, num1, digits=6):
     n0 = calc_rounded_str_value(num0, digits=digits)
     n1 = calc_rounded_str_value(num1, digits=digits)
     return n0 == n1
+
+
+# TODO :: cairo coords are all float!
+def translate_scale(x_pct, y_pct, scale=1.0, center_x=0.5, center_y=0.5):
+    if scale == 1.0:
+        # 1.0 == 1.0 + 1e-16 --> True
+        return (x_pct, y_pct)
+    else:
+        # 1.0 == 1.0 + 1e-15 --> False
+        x = scale * (x_pct - center_x) + center_x
+        y = scale * (y_pct - center_y) + center_y
+        return (x, y)
+
+# TODO :: cairo coords are all float!
+def calc_default_line_width(size_pix):
+    # return math.ceil( 1 + ( 6 * size_pix / 5000 ) )
+    return 0.04
+
 
 def calc_straight_line(start, end):
     x0, y0 = floatify_iterable(start)
@@ -127,51 +145,84 @@ def calc_line_length(point, other):
     sqr_dist = (x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0)
     return math.sqrt(sqr_dist)
 
-if __name__ == '__main__':
-    print
-    print floatify_iterable( (1, 2, 3) )
-    print floatify_iterable( [1, 2, 3] )
-    print
 
-    print
-    print q_same_rounded_floats(3.0, 3.0)
-    print q_same_rounded_floats(3.0, -3.0)
-    print q_same_rounded_floats(3.14, math.pi)
-    print q_same_rounded_floats(3.1415926535, math.pi)
-    print q_same_rounded_floats(3.1415, math.pi, digits=3)
-    print
+# TODO :: cairo coords are all float!
+class RectanglePix:
+    def __init__(self, x_pix, y_pix, width_pix, height_pix):
+        assert isinstance(x_pix, int)
+        assert isinstance(y_pix, int)
+        assert isinstance(width_pix, int)
+        assert isinstance(height_pix, int)
+
+        self.x_pix = x_pix
+        self.y_pix = y_pix
+        self.width_pix = width_pix
+        self.height_pix = height_pix
+
+    def as_tuple(self):
+        return (self.x_pix, self.y_pix, self.width_pix, self.height_pix)
+
+    @staticmethod
+    def from_tuple(tpl):
+        return RectanglePix( *tpl[ 0 : 4 ] )
+
+    def calc_point(self, x_pct, y_pct):
+        x_pix = self.x_pix + x_pct * self.width_pix
+        # y = self.top + (1.0 - y_pct) * self.height
+        y_pix = self.y_pix + y_pct * self.height_pix
+        return (int(x_pix), int(y_pix))
+
+    def calc_size(self, width_pct, height_pct):
+        width_pix  = width_pct * self.width_pix
+        height_pix  = height_pct * self.height_pix
+        return (int(width_pix), int(height_pix))
+
+
+if __name__ == '__main__':
+    print()
+    print( floatify_iterable( (1, 2, 3) ) )
+    print( floatify_iterable( [1, 2, 3] ) )
+    print()
+
+    print()
+    print( q_same_rounded_floats(3.0, 3.0) )
+    print( q_same_rounded_floats(3.0, -3.0) )
+    print( q_same_rounded_floats(3.14, math.pi) )
+    print( q_same_rounded_floats(3.1415926535, math.pi) )
+    print( q_same_rounded_floats(3.1415, math.pi, digits=3) )
+    print()
 
     start0 = (3, 2)
     end0 = (7, 5)
     dist0 = 2.0
-    print start0, end0, dist0, calc_distant_points_on_inverse_line(start0, end0, dist0)
-    print end0, start0, dist0, calc_distant_points_on_inverse_line(end0, start0, dist0)
-    print
+    print( start0, end0, dist0, calc_distant_points_on_inverse_line(start0, end0, dist0) )
+    print( end0, start0, dist0, calc_distant_points_on_inverse_line(end0, start0, dist0) )
+    print()
 
     # start0 = (3, 2)
     # end0 = (7, 5)
     dist0 = 5.0
-    print start0, end0, dist0, calc_distant_points_on_inverse_line(start0, end0, dist0)
-    print end0, start0, dist0, calc_distant_points_on_inverse_line(end0, start0, dist0)
-    print
+    print( start0, end0, dist0, calc_distant_points_on_inverse_line(start0, end0, dist0) )
+    print( end0, start0, dist0, calc_distant_points_on_inverse_line(end0, start0, dist0) )
+    print()
 
     start1 = (3.0, 2.0)
     end1 = (3.0, 5.0)
     dist1 = 1.42
-    print start1, end1, dist1, calc_distant_points_on_inverse_line(start1, end1, dist1)
-    print end1, start1, dist1, calc_distant_points_on_inverse_line(end1, start1, dist1)
-    print
+    print( start1, end1, dist1, calc_distant_points_on_inverse_line(start1, end1, dist1) )
+    print( end1, start1, dist1, calc_distant_points_on_inverse_line(end1, start1, dist1) )
+    print()
 
     start2 = (3.0, 2.0)
     end2 = (7.0, 2.0)
     dist2 = 1.608
-    print start2, end2, dist2, calc_distant_points_on_inverse_line(start2, end2, dist2)
-    print end2, start2, dist2, calc_distant_points_on_inverse_line(end2, start2, dist2)
-    print
+    print( start2, end2, dist2, calc_distant_points_on_inverse_line(start2, end2, dist2) )
+    print( end2, start2, dist2, calc_distant_points_on_inverse_line(end2, start2, dist2) )
+    print()
 
     start3 = (2, 1)
     end3 = (5, 7)
     dist3 = 3.5
-    print start3, end3, dist3, calc_division_point(start3, end3, dist3)
-    print end3, start3, dist3, calc_division_point(end3, start3, dist3)
-    print
+    print( start3, end3, dist3, calc_division_point(start3, end3, dist3) )
+    print( end3, start3, dist3, calc_division_point(end3, start3, dist3) )
+    print()
