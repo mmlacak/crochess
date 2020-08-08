12affb03d54083c07ae505dceec004b74069cd58
Mario Mlačak <mmlacak@gmail.com>
2020-08-08 03:57:01 +0200

    Added utility function.

--- book/py/util.py
+++ book/py/util.py
@@ -72,3 +72,34 @@ def in_range(v, min_v, max_v, include_min=True, include_max=True):
         return include_max
 
     return True
+
+
+def convert_to_tuple(color_str):
+    assert isinstance(color_str, str)
+    assert len(color_str) == 7
+    assert color_str[0] == '#'
+
+    r_str, g_str, b_str = color_str[1:3], color_str[3:5], color_str[5:7]
+    r, g, b = int(r_str, base=16), int(g_str, base=16), int(b_str, base=16)
+    return ( r / 255.0, g / 255.0, b / 255.0 )
+
+
+def test_1():
+
+    def print_color_and_tuple(color_str):
+        print
+        print( color_str )
+        print( convert_to_tuple( color_str ) )
+
+    print_color_and_tuple( "#123456" )
+    print_color_and_tuple( '#4080C0' )
+    print_color_and_tuple( '#3F7FBF' )
+
+    print_color_and_tuple( "#AABBCC" )
+    print_color_and_tuple( '#DDEEFF' )
+
+    print
+
+
+if __name__ == '__main__':
+    test_1()
