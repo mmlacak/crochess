c1bd81dc3fdf26540e9bb182f6119928519b02b7
Mario Mlačak <mmlacak@gmail.com>
2021-02-27 14:20:24 +0100

    Added main loop, help commands.

--- ws/crochess/src/main.rs
+++ ws/crochess/src/main.rs
@@ -1,6 +1,9 @@
 // Copyright (c) 2021 Mario Mlačak, mmlacak@gmail.com
 // Licensed under 3-clause (modified) BSD license. See LICENSE for details.
 
+use std::io;
+use std::io::Write;
+
 use libcrochess as libcc;
 // use libcrochess::piece_type as pt;
 use libcrochess::piece_type::PieceType as PT;
@@ -68,4 +71,109 @@ fn main() {
     // libcc::dbgv( &bb.piece_at(3, 5) );
     // libcc::dbgv( &bb );
     println!( "{}", bb );
+
+
+    loop {
+        let mut input = String::new();
+
+        print!( "> " );
+        io::stdout().flush();
+        io::stdin().read_line(&mut input);
+
+        let args: Vec<&str> = input.trim().split_whitespace().collect();
+        if 0 < args.len() {
+            let cmd = args[0];
+
+            match cmd {
+                "q" | "quit" => { break; }
+                "h" | "help" | "?" => {
+                    println!( "Croatian chess - console application
+Copyright (c) 2021 Mario Mlačak, mmlacak@gmail.com.
+Licensed under 3-clause (modified) BSD license. Use a(bout) command for details.
+
+Based on book 'Croatian chess and other variants', by Mario Mlačak.
+
+Commands:
+h, help       - prints this screen
+a, about      - prints about info
+v, version    - prints version(s) info
+q, quit       - quits program
+d, display    - display current positions
+* i, info     - display list of all moves played, time
+* t, time     - (re)sets time counter(s)
+n, new        - starts new game, keeps variant
+                to change variant use code from table below, e.g. n ct
+* p, players  - sets up players
+                takes two parameters, both are one of 'bot', 'human'
+* m, move     - moves piece(s)
+                takes notation as parameter, e.g. m Nc3
+* s, save     - saves current game into PGN file
+                takes <path> as parameter, e.g. s my_new_game.pgn
+* l, load     - loads game/positions from PGN file
+                takes <path> as parameter, e.g. l my_new_game.pgn
+
+Commands marked with * are not currently implemented.
+
+Supported variants (to be used as parameter to 'new' command):
+cc  - Classical
+ct  - Croatian Ties
+ma  - Mayan Ascendancy
+aoa - Age Of Aquarius
+mv  - Miranda's Veil
+n   - Nineteen
+hd  - Hemera's Dawn
+tr  - Tamoanchan Revisited
+cot - Conquest Of Tlalocan
+d   - Discovery
+o   - One" );
+                }
+                "a" | "about" => {
+                    println!( "Copyright (c) 2021 Mario Mlačak, mmlacak@gmail.com
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+\"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." );
+                }
+                "v" | "version" => {
+                    println!( "Croatian chess - console application
+Copyright (c) 2021 Mario Mlačak, mmlacak@gmail.com.
+Licensed under 3-clause (modified) BSD license. Use a(bout) command for details.
+
+2021-2-27: ver. 0.1.0.945
+Initial public hosting, more for backup than for public useage." );
+                }
+                _ => { println!("You typed: {}", input.trim()); }
+            }
+        }
+
+        // println!("You typed: {}", input.trim());
+
+        // for s in args.iter() {
+        //     println!("You typed: {}", s);
+        // }
+    }
 }
