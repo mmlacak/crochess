86fdabcb441b5e76c97135ce3481a69565f74789
Mario Mlačak <mmlacak@gmail.com>
2021-07-21 19:23:59 +0200

    Documented tags.

--- ws/libcrochess/inc/cc_tag.h
+++ ws/libcrochess/inc/cc_tag.h
@@ -5,21 +5,44 @@
 #define __CC_TAG_H__
 
 
+/**
+    @file cc_tag.h
+    @brief Tag enumeration, and related functions.
+
+    Tag is a link between a piece and field at which it stands.
+    Every piece can have only one tag applied at any given time.
+
+    Persistant tags are valid until used or lost, e.g. until piece
+    is moved, activated, converted, captured, displaced, teleported,
+    demoted (if figure), promoted (if Pawn).
+*/
+
+/**
+    Enumerates all tags, used in all variants.
+
+    Tag `CC_TE_None` is used for e.g. empty on-board fields, any off-board field.
+*/
 typedef enum CcTagEnum
 {
-    CC_TE_None,
+    CC_TE_None, /**< No tag applies. */
 
-    // Persistant, i.e. valid until lost.
-    CC_TE_CanRush, // Pawns
-    CC_TE_CanCastle, // Rooks, Kings
-    CC_TE_DelayedPromotion, // Pawn
+    CC_TE_CanRush, /**< Pawn can rush, persistant tag. */
+    CC_TE_CanCastle, /**< Rooks, Kings can castle, persistant tag. */
+    CC_TE_DelayedPromotion, /**< Pawn delayed promotion, persistant tag. */
 
-    CC_TE_EnPassant, // Gained in a move, used or lost in next.
+    CC_TE_EnPassant, /**< En passant tag, semi-persistant. Gained in a move, used or lost in the very next one. */
 
-    CC_TE_PawnSacrifice, // Gained in a move, used or lost in the very same move.
+    CC_TE_PawnSacrifice, /**< Pawn sacrifice tag, non-persistant. Gained in a move, used or lost in the very same move. */
 } CcTagEnum;
 
 
+/**
+    Function returning tag char, based on tag enum.
+
+    @param ct Tag enum.
+
+    @return Tag char, uppercase if valid, space otherwise.
+*/
 char cc_tag_as_char( CcTagEnum const ct );
 
 
