1c4b0841db7d9a24cf22d5a0cba2b51ae827454b
Mario Mlačak <mmlacak@gmail.com>
2021-08-21 02:14:32 +0200

    Simplified defaulting of output arguments.

--- ws/libcrochess/src/cc_parse_utils.c
+++ ws/libcrochess/src/cc_parse_utils.c
@@ -539,15 +539,10 @@ bool cc_parse_utils_get_fields( char const * const restrict fields_str,
 
     if ( !cc_parse_utils_is_fields_str_valid( fields_str ) ) return false;
 
-    *disambiguation_file_o = CC_INVALID_OFF_BOARD_COORD_MIN;
-    *disambiguation_rank_o = CC_INVALID_OFF_BOARD_COORD_MIN;
-    *file_o = CC_INVALID_OFF_BOARD_COORD_MIN;
-    *rank_o = CC_INVALID_OFF_BOARD_COORD_MIN;
-
-    int file_0 = CC_INVALID_OFF_BOARD_COORD_MIN;
-    int rank_0 = CC_INVALID_OFF_BOARD_COORD_MIN;
-    int file_1 = CC_INVALID_OFF_BOARD_COORD_MIN;
-    int rank_1 = CC_INVALID_OFF_BOARD_COORD_MIN;
+    int file_0 = *disambiguation_file_o = CC_INVALID_OFF_BOARD_COORD_MIN;
+    int rank_0 = *disambiguation_rank_o = CC_INVALID_OFF_BOARD_COORD_MIN;
+    int file_1 = *file_o = CC_INVALID_OFF_BOARD_COORD_MIN;
+    int rank_1 = *rank_o = CC_INVALID_OFF_BOARD_COORD_MIN;
 
     char const * file_0_str = fields_str;
     if ( *file_0_str != '\0' )
--- ws/libcrochess/src/cc_version.c
+++ ws/libcrochess/src/cc_version.c
@@ -11,4 +11,4 @@
 */
 
 
-char const CC_LIB_VERSION[] = "0.0.2.15:214+20210820.234535"; // source-new-libcrochess-version-major-minor-feature-commit+meta~breaks-place-marker
+char const CC_LIB_VERSION[] = "0.0.2.16:215+20210821.001432"; // source-new-libcrochess-version-major-minor-feature-commit+meta~breaks-place-marker
