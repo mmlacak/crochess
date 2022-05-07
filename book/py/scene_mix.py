28c4fe77ad5038dcd68e986d0194edc4b1970b22
Mario Mlačak <mmlacak@gmail.com>
2022-05-07 12:32:45 +0200

    Added Pyramid activated by sideways Pawn examples.

--- book/py/scene_mix.py
+++ book/py/scene_mix.py
@@ -35,5 +35,7 @@ class SceneMix(SceneMixin, \
     def _get_recent_scene_method_names(self):
         return  [
                     # 'scn_n_15_sideways_pawn_init', \
-                    'scn_n_16_sideways_pawn_activated_wave', \
+                    # 'scn_n_16_sideways_pawn_activated_wave', \
+                    'scn_n_17_sideways_pawn_does_not_activate_pyramid', \
+                    'scn_n_18_sideways_pawns_do_activate_pyramids', \
                 ]
