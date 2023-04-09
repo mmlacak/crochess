5c9678e1070d0bdac3905aa7178614688cb279d4
Mario Mlačak <mmlacak@gmail.com>
2023-04-09 10:53:46 +0200

    Added Serpent step limit example; fixed formula.

--- book/py/scene_mix.py
+++ book/py/scene_mix.py
@@ -34,7 +34,9 @@ class SceneMix(SceneMixin, \
     # overrides
     def _get_recent_scene_method_names(self):
         return  [
-                    'scn_tr_09_serpent_loop_init', \
-                    'scn_tr_10_serpent_loop_step', \
-                    'scn_tr_11_serpent_loop_end', \
+                    # 'scn_tr_09_serpent_loop_init', \
+                    # 'scn_tr_10_serpent_loop_step', \
+                    # 'scn_tr_11_serpent_loop_end', \
+                    'scn_tr_15_serpent_step_limit', \
+                    'scn_tr_16_serpent_step_limit_momentum', \
                 ]
