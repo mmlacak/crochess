1d414370095e56764c0c30c709a29bb34af2c1ce
Mario Mlačak <mmlacak@gmail.com>
2023-11-06 20:47:40 +0100

    Commented out position link struct, functions.

--- ws/crochess/src/crochess.c
+++ ws/crochess/src/crochess.c
@@ -26,7 +26,7 @@
 #include "crochess.h"
 
 
-char const CROCHESS_VERSION[] = "0.0.1.380:812+20231106.194634"; // source-new-crochess-version-major-minor-feature-commit+meta~breaks-place-marker
+char const CROCHESS_VERSION[] = "0.0.1.381:813+20231106.194740"; // source-new-crochess-version-major-minor-feature-commit+meta~breaks-place-marker
 
 #ifdef __WITH_LINE_NOISE__
 char const CROCHESS_HISTORY_FILE_NAME[] = "history_crochess.txt";
--- ws/libcrochess/src/cc_version.c
+++ ws/libcrochess/src/cc_version.c
@@ -11,4 +11,4 @@
 */
 
 
-char const CC_LIB_VERSION[] = "0.0.1.380:812+20231106.194634"; // source-new-libcrochess-version-major-minor-feature-commit+meta~breaks-place-marker
+char const CC_LIB_VERSION[] = "0.0.1.381:813+20231106.194740"; // source-new-libcrochess-version-major-minor-feature-commit+meta~breaks-place-marker
